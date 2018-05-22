1518af132a841ae363f7711760a087cd0dfbed58
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-05-21 23:13:45 -0600

    Checkpoint3-Create-Chat-Rooms:Submission1

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,17 +3,40 @@ import React, { Component } from 'react';
 class RoomList extends Component {
   constructor(props) {
     super(props);
-    this.state = { rooms: [] };
+    this.state = {
+      rooms: [],
+      value: ''
+    };
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
      this.roomsRef.on('child_added', snapshot => {
        const room = snapshot.val();
-       // console.log(snapshot);
        room.key = snapshot.key;
        this.setState({ rooms: this.state.rooms.concat( room ) });
      });
+   }
+
+   createRoom (room) {
+     this.roomsRef.push({
+       name: room
+     });
+   }
+
+   handleChange(e) {
+    this.setState({ value: e.target.value})
+  }
+
+   handleSubmit(e) {
+     e.preventDefault();
+     this.createRoom(this.state.value);
+     this.setState({ value: ''});
+     // if (!this.state.value) { return }
+     // const newRoom = { name: this.state.value, isCompleted: false };
+     // this.setState({ rooms: [...this.state.rooms, newRoom], value: ''});
    }
 
     render() {
@@ -24,7 +47,15 @@ class RoomList extends Component {
               <ul>{room.name}</ul>
             </div>
           )}
+          <form onSubmit={ this.handleSubmit }>
+            <label>
+              New Room Name:
+              <input type="text" value={ this.state.value } onChange={ this.handleChange } />
+            </label>
+              <input type="submit" value="Submit" />
+          </form>
         </div>
+
      );
    }
  }
